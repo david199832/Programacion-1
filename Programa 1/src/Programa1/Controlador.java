cab1a7a9eb81be616a39730453b688836570618d
mas_6 <mas_6@LAPTOP-ON3QR9TP>
2018-09-08 11:53:51 -0500

    Primera tarea

---
+++ Programa 1/src/Programa1/Controlador.java
@@ -0,0 +1,11 @@
+package Programa1;
+
+import javax.swing.JOptionPane;
+
+public class Controlador {
+
+	public static void main (String [] args) {
+		JOptionPane.showMessageDialog(null, new NumeroLetras().convertir(87));
+	}
+}
+
