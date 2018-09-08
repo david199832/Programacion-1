b0d2e074b1e9d1dc27d8fa04bd5291ced3c71872
mas_6 <mas_6@LAPTOP-ON3QR9TP>
2018-09-08 11:55:05 -0500

    Calculo

---
+++ trabajo1/src/Calculo/calculo.java
@@ -0,0 +1,34 @@
+package Calculo;
+
+public class calculo
+{
+
+	
+	private int numero;
+	
+	public calculo(int pNumero)
+	{
+		
+		numero = pNumero;
+		
+		
+	}
+	
+	
+public String convertir(int pNumero)
+{
+	String num = null; 
+	
+		
+	{
+	
+		
+	}
+	
+	
+	return num;
+}
+
+
+
+}
---
+++ trabajo1/src/Calculo/controlador.java
@@ -0,0 +1,26 @@
+package Calculo;
+
+import java.util.Scanner;
+
+public class controlador
+{
+public static void main(String[] args) {
+	
+	int numero; 
+	
+	Scanner calculo = new Scanner(System.in);
+	
+	System.out.println("bienvenido al calculador \n ingrese el numero deseado");
+	
+	numero = Integer.parseInt(calculo.nextLine());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
