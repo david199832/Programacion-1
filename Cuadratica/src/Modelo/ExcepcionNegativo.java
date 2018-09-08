2b99e047d1a4c16fb5d0e35d3de0f98aa8730c7a
mas_6 <mas_6@LAPTOP-ON3QR9TP>
2018-09-08 11:45:41 -0500

    Taller Cuadratica

---
+++ Cuadratica/src/Modelo/ExcepcionNegativo.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+public class ExcepcionNegativo extends Exception {
+	 
+	private static final long serialVersionUID = 1L;
+ 
+	public ExcepcionNegativo(){
+		super();
+	}
+	public ExcepcionNegativo(String msg){
+		super(msg);
+	}
+}
